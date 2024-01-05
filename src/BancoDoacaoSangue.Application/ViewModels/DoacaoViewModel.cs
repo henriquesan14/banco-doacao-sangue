d84d9c3b20efb6b94343b6134ba9b59fc14ca684
henrique <henriqueunipe@gmail.com>
2024-01-04 21:24:33 -0300

    Ajuste nos views models

--- src/BancoDoacaoSangue.Application/ViewModels/DoacaoViewModel.cs
+++ src/BancoDoacaoSangue.Application/ViewModels/DoacaoViewModel.cs
@@ -2,6 +2,7 @@
 {
     public class DoacaoViewModel
     {
+        public int Id { get; set; }
         public int? QuantidadeMl { get; set; }
         public DateTime CriadoEm { get; set; }
     }
--- src/BancoDoacaoSangue.Application/ViewModels/RelatorioDoacaoViewModel.cs
+++ src/BancoDoacaoSangue.Application/ViewModels/RelatorioDoacaoViewModel.cs
@@ -4,6 +4,7 @@ namespace BancoDoacaoSangue.Application.ViewModels
 {
     public class RelatorioDoacaoViewModel
     {
+        public int Id { get; set; }
         public virtual Doador? Doador { get; set; }
         public int? QuantidadeMl { get; set; }
         public DateTime CriadoEm { get; set; }
